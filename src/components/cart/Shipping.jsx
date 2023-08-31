09f5385064b43df8e83f97b918dc9d618b73b9ba
nur.sakifah@gmail.com <nur.sakifah@gmail.com>
2023-09-01 00:46:12 +0200

    all exercises done

--- src/components/cart/Shipping.jsx
+++ src/components/cart/Shipping.jsx
@@ -1,52 +1,65 @@
 import React from "react";
-import { Country, State } from "country-state-city";
+import { State } from "country-state-city";
 import Popup from 'reactjs-popup';
+import { Link } from "react-router-dom";
 
-const Shipping = () => {
+const Shipping = () => { 
   return (
-    <section className="shipping">
+    <section className="shipping"> 
       <main>
-        <h1>Shipping Details</h1>
+        <h1>Détails de la livraison</h1> 
         <form>
           <div>
-            <label>H.No.</label>
-            <input type="text" placeholder="Enter House No." />
-          </div>
+            <label>Adresse</label>
+            <input type="text" placeholder="Introduisez votre adresse" />
+          </div> 
           <div>
-            <label>City</label>
-            <input type="text" placeholder="Enter City" />
+            <label>Ville</label>
+            <input type="text" placeholder="Introduisez votre ville" /> 
           </div>
-          <div>
-              {/* Compelte the code for the COUNTRY DROPDOWN*/}
-            <label>Country</label>
-
+          <div> 
+            <label>Pays</label>
             <select>
-              <option value="">Country</option>
-// Enter the code here for country dropdown           
-                  </option>
-                ))}
+              <option value="">Inde</option> {}
             </select>
           </div>
-          <div>
-              {/* Add the code for the STATE DROPDOWN*/}
-           
-          </div>
-          <div>
-            <label>Pin Code</label>
-            <input type="number" placeholder="Enter Pincode" />
-          </div>
-        // Enter thr code for contact           
-          
-          <Popup trigger=
-                {<button type = "button">Confirm Order</button>}
-                position="right center">
-                <div style={{color:"red",position: 'absolute', top: '50%', right: '100%', transform: 'translateY(-50%)', backgroundColor: '#fff', padding: '10px', borderRadius: '5px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)'}}>Order Placed</div>
-               
+          <div> 
+            <label>État/Région</label>
+            <select>
+                <option value="">État/Région</option> 
+                  {State && State.getStatesOfCountry("IN").map((i) => ( 
+                  <option value={i.isoCode} key={i.isoCode}>{i.name}</option>))}
+              </select>
+            </div> 
+            <div>
+              <label>Code postal</label>
+              <input type="number" placeholder="Introduisez votre code postal" /> 
+            </div>
+            <div>
+              <label>N° de téléphone</label>
+              <input type="number" placeholder="Introduisez votre n° de téléphone" />
+            </div>
+            <Popup
+              trigger={
+                <Link className="link" to="/myorders"> 
+                    Confirmer la commande
+                </Link> }> 
+                <div
+                  style={{ 
+                    color:"red", 
+                    transform: "translate(0%,-500%)", 
+                    backgroundColor: "#fff",
+                    padding: "10px",
+                    borderRadius: "5px",
+                    boxShadow: "0 0 10px rgba(0, 0, 0, 0.2)",
+                  }} >
+                  Votre commande est en cours de traitement ! 
+                </div>
             </Popup>
-        </form>
-      </main>
-    </section>
-  );
+          </form>
+        </main>
+      </section>
+  ); 
 };
-
+  
 export default Shipping;
--- src/components/myOrders/MyOrders.jsx
+++ src/components/myOrders/MyOrders.jsx
@@ -21,8 +21,19 @@ const MyOrders = () => {
           </thead>
 
           <tbody>
-//     Add the code for the table body
-           
+            {arr.map((i) => (
+              <tr key={i}>
+                <td>#sdkfsdfdsf</td>
+                <td>En cours de traitement</td> <td>23</td>
+                <td>${2132}</td>
+                <td>CB</td>
+                <td>
+                  <Link to={`/order/${"asdsds"}`}> 
+                    <AiOutlineEye />
+                  </Link>
+                  </td>
+              </tr> 
+              ))}
           </tbody>
         </table>
       </main>
